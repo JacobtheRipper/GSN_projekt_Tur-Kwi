230b6507d4b95e816a9c4ccef1892237b66b2e34
JacobtheRipper <95055029+JacobtheRipper@users.noreply.github.com>
2021-12-11 20:26:35 +0100

    Poprawka indentacji - użycie tabulacji zamiast spacji

--- convnet.py
+++ convnet.py
@@ -10,24 +10,24 @@ import torchvision.transforms as transforms # Transformations that can be perfor
 # "Exploring Data Augmentation to Improve Music Genre Classification with ConvNets" article
 # the input of this CNN is a grayscale 256x16 image patch derived from the spectrogram
 class ConvNet(nn.Module):
-    def __init(self, input_channels=1, num_classes=8):
-        super(ConvNet, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=1, kernel_size=(5, 5), stride=1, padding=2)  # output's shape is equal to its input shape
-        self.max_pool = nn.MaxPool2d(kernel_size=(2, 2), stride=2)  # downsampling from 256x16 to 128x8 or from 128x8 to 64x4
-	self.conv2 = nn.Conv2d(in_channels=1, out_channels=1, kernel_size=(5, 5), stride=1, padding=2)  # same as conv1
-	self.fc1 = nn.Linear(in_features=64*4*1, out_features=500)  # in_features = image_size*out_channels
-	self.fc2 = nn.Linear(in_features=500, out_features=num_classes)
+	def __init(self, input_channels=1, num_classes=8):
+		super(ConvNet, self).__init__()
+		self.conv1 = nn.Conv2d(in_channels=1, out_channels=1, kernel_size=(5, 5), stride=1, padding=2)  # output's shape is equal to its input shape
+		self.max_pool = nn.MaxPool2d(kernel_size=(2, 2), stride=2)  # downsampling from 256x16 to 128x8 or from 128x8 to 64x4
+		self.conv2 = nn.Conv2d(in_channels=1, out_channels=1, kernel_size=(5, 5), stride=1, padding=2)  # same as conv1
+		self.fc1 = nn.Linear(in_features=64*4*1, out_features=500)  # in_features = image_size*out_channels
+		self.fc2 = nn.Linear(in_features=500, out_features=num_classes)
     
-    def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = self.max_pool(x)
-	x = F.relu(self.conv2(x))
-        x = self.max_pool(x)
-	x = x.reshape(x.shape[0], -1)
-	x = self.fc1(x)
-	x = F.softmax(x)
-	x = self.fc2(x)
-        return x
+	def forward(self, x):
+		x = F.relu(self.conv1(x))
+		x = self.max_pool(x)
+		x = F.relu(self.conv2(x))
+		x = self.max_pool(x)
+		x = x.reshape(x.shape[0], -1)
+		x = self.fc1(x)
+		x = F.softmax(x)
+		x = self.fc2(x)
+	return x
 
 # Set device
 device = torch.device('cuda' if torch.cuda_is_available() else 'cpu')
@@ -71,48 +71,48 @@ optimiser = optim.Adam(model.parameters(), lr=learning_rate)
 # Train CNN
 #TODO create a train_model() function
 for epochs in range(num_epochs):
-    for batch_idx, (data, targets) in enumerate(train_data_loader):
-        #Get data to Cuda if possible
-        data = data.to(device=device)
-        targets = targets.to(device=device)
+	for batch_idx, (data, targets) in enumerate(train_data_loader):
+	#Get data to Cuda if possible
+	data = data.to(device=device)
+	targets = targets.to(device=device)
         
-        #forward propagation
-        scores = model(data)
-        loss = criterion(scores, targets)
-        
-        #backward propagation
-        optimiser.zero_grad()
-        loss.backward()
-        
+	#forward propagation
+	scores = model(data)
+	loss = criterion(scores, targets)
+
+	#backward propagation
+	optimiser.zero_grad()
+	loss.backward()
+
         #gradient descent
-        optimiser.step()
+	optimiser.step()
         
 # Check accuracy on training & test data
 
 def check_accuracy(loader, model):
-    if loader.dataset.train:
-        print("Checking accuracy on training data")
-    else:
-        print("Checking accuracy on test data")
+	if loader.dataset.train:
+		print("Checking accuracy on training data")
+	else:
+		print("Checking accuracy on test data")
 
-    num_correct = 0
-    num_samples = 0
-    model.eval()
+	num_correct = 0
+	num_samples = 0
+	model.eval()
 
-    with torch.no_grad():
-        for x, y in loader:
-            x = x.to(device=device)
-            y = y.to(device=device)
-            x = x.reshape(x.shape[0], -1)
+	with torch.no_grad():
+		for x, y in loader:
+			x = x.to(device=device)
+			y = y.to(device=device)
+			x = x.reshape(x.shape[0], -1)
 
-            scores = model(x)
-            _, predictions = scores.max(1)
-            num_correct += (predictions == y).sum()
-            num_samples += predictions.size(0)
+			scores = model(x)
+			_, predictions = scores.max(1)
+			num_correct += (predictions == y).sum()
+			num_samples += predictions.size(0)
 
-        print(f'Correctly classified examples: {num_correct} / {num_samples} with accuracy {float(num_correct)/float(num_samples)*100:.2f}')
+	print(f'Correctly classified examples: {num_correct} / {num_samples} with accuracy {float(num_correct)/float(num_samples)*100:.2f}')
 
-    model.train()
+	model.train()
 
 check_accuracy(train_data_loader, model)
 check_accuracy(test_data_loader, model)
