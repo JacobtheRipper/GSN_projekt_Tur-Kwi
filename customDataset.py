0677988ec3ba81775486d6a1325cb96d26ee5be5
JacobtheRipper <95055029+JacobtheRipper@users.noreply.github.com>
2021-12-15 21:48:53 +0100

    Adding a class for a custom dataset

---
+++ customDataset.py
@@ -0,0 +1,26 @@
+import os
+import pandas as pd  # CSV file parsing
+from torch.utils.data import Dataset
+from torch import tensor
+from skimage import io
+
+class customDataset(Dataset):
+    def __init__(self, annotation_file, data_dir, transform=None):
+        self.annotation = pd.read_csv(annotation_file)
+        self.data_dir = data_dir
+        self.transform = transform
+
+    # returns the size of the dataset
+    def __len__(self):
+        return len(self.annotation)
+
+    def __getItem__(self, index):
+        # TODO dataset path should be set for this code to work, also index 0 should contain images' names
+        img_dir = os.path.join(self.data_dir, self.annotation.iloc[index, 0])
+        image = io.imread(img_dir)
+        label = tensor(int(self.annotation.iloc[index, 1]))
+
+        if self.transform:
+            image = self.transform(image)
+
+        return (image, label)
