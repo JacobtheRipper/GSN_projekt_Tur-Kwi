35dcc3991c73de0e92b1a26c779a1a2f06cdb22e
JacobtheRipper <95055029+JacobtheRipper@users.noreply.github.com>
2021-12-17 21:11:56 +0100

    Quick fix for Value Error
    ValueError: invalid literal for int() with base 10: 'Hip-Hop'

--- customDataset.py
+++ customDataset.py
@@ -17,9 +17,31 @@ class customDataset(Dataset):
     def __getitem__(self, index):
         img_dir = os.path.join(self.data_dir, self.annotation.iloc[index, 0])
         image = io.imread(img_dir)
-        label = tensor(int(self.annotation.iloc[index, 1]))
+        
+        str_label = self.annotation.iloc[index, 1]
+        # Convert string labels to integers and then tensors
+        label = tensor(string_to_int_label(str_label))
 
         if self.transform:
             image = self.transform(image)
 
         return (image, label)
+    
+    # Convert string labels to integers
+    def string_to_int_label(str):
+        if str == "Electronic":
+            return 0
+        if str == "Experimental":
+            return 1
+        if str == "Folk":
+            return 2
+        if str == "Hip-Hop":
+            return 3
+        if str == "Instrumental":
+            return 4
+        if str == "International":
+            return 5
+        if str == "Pop":
+            return 6
+        if str == "Rock":
+            return 7
